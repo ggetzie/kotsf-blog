75cddae4c524e45c688d2ddeb01ef2af7e9076fe
gabe <ggetzie@gmail.com>
2021-11-30 07:56:35 +0800

    referrals css

--- src/pages/referrals.js
+++ src/pages/referrals.js
@@ -3,30 +3,32 @@ import Layout from "../components/layout"
 import { graphql } from "gatsby"
 import { Helmet } from "react-helmet"
 
-export default function Referrals ({ data }) {
-    const {markdownRemark: post} = data
-    return ( 
-        <Layout>
-            <Helmet title={data.site.siteMetadata.title} />
-            <h2>{post.frontmatter.title}</h2>
-            <div className="blogpost referrals"
-                 dangerouslySetInnerHTML={{ __html: post.html }}/>
-        </Layout>
-    )
+export default function Referrals({ data }) {
+  const { markdownRemark: post } = data
+  return (
+    <Layout>
+      <Helmet title={data.site.siteMetadata.title} />
+      <h2>{post.frontmatter.title}</h2>
+      <div
+        className="referrals"
+        dangerouslySetInnerHTML={{ __html: post.html }}
+      />
+    </Layout>
+  )
 }
 
 export const query = graphql`
-    query {
-        markdownRemark(frontmatter: {path: {eq: "/referrals/"}}) {
-            html
-            frontmatter {
-                title
-            }
-        }
-        site {
-            siteMetadata {
-                title
-            }
-        }
+  query {
+    markdownRemark(frontmatter: { path: { eq: "/referrals/" } }) {
+      html
+      frontmatter {
+        title
+      }
     }
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
 `
