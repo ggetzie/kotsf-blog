1da3820b6deb367d6edcbe1d3f6607f0f80022b9
Gabriel Getzie <gabe@kotsf.com>
2022-09-04 21:39:47 +0800

    switch about page to typescript

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,23 +2,24 @@ import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
 import { Helmet } from "react-helmet"
+import type { PageProps } from "gatsby"
 
-export default function About({ data }) {
+export default function About({ data }: PageProps<Queries.aboutPageQuery>) {
   const { markdownRemark: post } = data
   return (
     <Layout>
-      <Helmet title={"About - " + data.site.siteMetadata.title} />
-      <h2>{post.frontmatter.title}</h2>
+      <Helmet title={"About - " + data.site?.siteMetadata?.title} />
+      <h2>{post?.frontmatter?.title}</h2>
       <div
         className="blogpost"
-        dangerouslySetInnerHTML={{ __html: post.html }}
+        dangerouslySetInnerHTML={{ __html: post?.html || "" }}
       />
     </Layout>
   )
 }
 
 export const pageQuery = graphql`
-  query aboutQuery {
+  query aboutPage {
     markdownRemark(frontmatter: { path: { eq: "/about/" } }) {
       html
       frontmatter {
