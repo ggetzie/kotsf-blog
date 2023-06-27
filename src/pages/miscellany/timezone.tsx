a656d9933d78a4f2ec34eda7fbb241e3f7e5eec7
Gabriel Getzie <ggetzie@hku.hk>
2023-06-27 16:11:46 +0800

    input datetime

--- src/pages/miscellany/timezone.tsx
+++ src/pages/miscellany/timezone.tsx
@@ -3,13 +3,44 @@ import { Helmet } from "react-helmet"
 import { graphql, PageProps } from "gatsby"
 import Layout from "../../components/layout"
 
+const toDateTimeString = (d: Date) => {
+  const year = d.getFullYear().toString()
+  const month = (d.getMonth() + 1).toString().padStart(2, "0")
+  const day = d.getDate().toString().padStart(2, "0")
+  const hour = d.getHours().toString().padStart(2, "0")
+  const minutes = d.getMinutes().toString().padStart(2, "0")
+
+  return `${year}-${month}-${day}T${hour}:${minutes}`
+}
+
 export default function Timezone({ data }: PageProps<Queries.TimezoneQuery>) {
+  const [inputDatetime, setInputDatetime] = useState<Date>(new Date())
+  console.log(toDateTimeString(inputDatetime))
   return (
     <Layout is_index={false}>
       <Helmet
         title={"Timezone Converter - " + data?.site?.siteMetadata?.title}
       />
       <h1>Timezone Converter</h1>
+
+      <h2 className="text-left">Input</h2>
+      <div className="row">
+        <div>
+          <label htmlFor="inputDatetime">Date and Time</label>
+        </div>
+        <div>
+          <input
+            id="inputDatetime"
+            className="form-control fc-horizontal"
+            type="datetime-local"
+            value={toDateTimeString(inputDatetime)}
+            style={{ width: "20em" }}
+            onChange={(e) => {
+              setInputDatetime(new Date(e.target.value))
+            }}
+          />
+        </div>
+      </div>
     </Layout>
   )
 }
