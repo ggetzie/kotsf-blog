3a7953d4e7f54eec28472c4b795d9ab5510e6a15
Gabriel Getzie <ggetzie@hku.hk>
2023-06-28 17:12:37 +0800

    converting timezones, added bootstrap

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,2 +1,3 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import "./src/styles/global.css"
 require("prismjs/themes/prism-okaidia.css")
--- src/pages/miscellany/timezone.tsx
+++ src/pages/miscellany/timezone.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Helmet } from "react-helmet"
 import { graphql, PageProps } from "gatsby"
 import Layout from "../../components/layout"
 import moment from "moment-timezone"
+
+import "../../styles/timezone.css"
 
 const toDateTimeString = (d: Date) => {
   const year = d.getFullYear().toString()
@@ -14,46 +16,116 @@ const toDateTimeString = (d: Date) => {
   return `${year}-${month}-${day}T${hour}:${minutes}`
 }
 
+const convertTZ = (inputDT: string, inputTZ: string, outputTZ: string) => {
+  if (!tzNames.includes(inputTZ)) {
+    return "Please select a valid input timezone from the list"
+  } else if (!tzNames.includes(outputTZ)) {
+    return "Please select a valid output timezone from the list"
+  } else {
+    const inputWithTZ = moment.tz(inputDT, inputTZ)
+    return inputWithTZ.tz(outputTZ).format("MMMM Do YYYY, h:mm A")
+  }
+}
+
+const tzNames = moment.tz.names()
+
+const TZList = () => {
+  return (
+    <datalist id="allTimezones">
+      {tzNames.map((tz, i) => (
+        <option key={i} value={tz}></option>
+      ))}
+    </datalist>
+  )
+}
+
+const TZInput = ({
+  id,
+  value,
+  onChange,
+}: {
+  id: string
+  value: string
+  onChange: (tz: string) => void
+}) => {
+  return (
+    <div className="mb-1">
+      <label htmlFor={id}>Timezone</label>
+      <input
+        id={id}
+        className="form-control"
+        list="allTimezones"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      <div id={`${id}help`} className="form-text">
+        Start typing the name of a timezone and select from the list.
+      </div>
+    </div>
+  )
+}
+
 export default function Timezone({ data }: PageProps<Queries.TimezoneQuery>) {
-  const [inputDatetime, setInputDatetime] = useState<Date>(new Date())
-  console.log(toDateTimeString(inputDatetime))
+  const now = toDateTimeString(new Date())
+  const defaultInputTZ = "Asia/Hong_Kong"
+  const defaultOutputTZ = "US/Eastern"
+  const [inputDatetime, setInputDatetime] = useState<string>(now)
+  const [inputTimezone, setInputTimezone] = useState<string>(defaultInputTZ)
+  const [outputTimezone, setOutputTimezone] = useState<string>(defaultOutputTZ)
+  const [outputDatetime, setOutputDatetime] = useState<string>(
+    convertTZ(now, defaultInputTZ, defaultOutputTZ)
+  )
+
+  useEffect(() => {
+    setOutputDatetime(convertTZ(inputDatetime, inputTimezone, outputTimezone))
+  }, [inputDatetime, inputTimezone, outputTimezone])
+
   return (
     <Layout is_index={false}>
       <Helmet
         title={"Timezone Converter - " + data?.site?.siteMetadata?.title}
       />
-      <h1>Timezone Converter</h1>
-
-      <h2 className="text-left">Input</h2>
-      <div className="row">
+      <TZList />
+      <div className="d-flex flex-column justify-content-between">
         <div>
-          <label htmlFor="inputDatetime">Date and Time</label>
+          <h1 className="text-center mb-5">Timezone Converter</h1>
+          <div className="tzSection">
+            <h2 className="mb-2">Input</h2>
+            <TZInput
+              id="inputTimezone"
+              value={inputTimezone}
+              onChange={(tz) => setInputTimezone(tz)}
+            />
+            <label htmlFor="inputDatetime">Date and Time</label>
+            <input
+              id="inputDatetime"
+              className="form-control"
+              type="datetime-local"
+              value={inputDatetime}
+              onChange={(e) => {
+                setInputDatetime(e.target.value)
+              }}
+            />
+          </div>
+          <h2 className="text-left">Output</h2>
+          <div className="tzSection">
+            <TZInput
+              id="outputTimezone"
+              value={outputTimezone}
+              onChange={(tz) => setOutputTimezone(tz)}
+            />
+          </div>
+          <p id="output" className="text-center fw-bold mt-3 fs-4">
+            {outputDatetime}
+          </p>
         </div>
-        <div>
-          <input
-            id="inputDatetime"
-            className="form-control fc-horizontal"
-            type="datetime-local"
-            value={toDateTimeString(inputDatetime)}
-            style={{ width: "20em" }}
-            onChange={(e) => {
-              setInputDatetime(new Date(e.target.value))
-            }}
-          />
-        </div>
+        {/* <p className="text-center mt-5">
+          <small>
+            Made with{" "}
+            <a href="https://momentjs.com/timezone/">Moment Timezone</a>
+          </small>
+        </p> */}
       </div>
-      <label htmlFor="inputTimezone">Timezone</label>
-      <select
-        id="inputTimezone"
-        className="form-control"
-        style={{ width: "15em" }}
-      >
-        {moment.tz.names().map((tzName, i) => (
-          <option key={i} value={tzName}>
-            {tzName}
-          </option>
-        ))}
-      </select>
     </Layout>
   )
 }
